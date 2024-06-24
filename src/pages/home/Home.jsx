8a6d7511d72dcbdfe318610f9254f1b2daca9a32
Daniel Kuzminski <dankuzmina@gmail.com>
2024-06-24 15:00:20 +0200

    git create personal trainer landing page first section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,4 @@
+//styles
 import "./home.css"
 
 export default function Home() {
@@ -5,25 +6,46 @@ export default function Home() {
 		<div className="landing">
 			{/* navbar */}
 			<section className="section1">
-                <div className="section1__left">
-
-                </div>
-                <div className="section1__right">
-                    <h1 className="section1__title">Postaw sobie wyzwanie</h1>
-                    <p className="section1__text">
-                        Czy marzysz o lepszej kondycji fizycznej, zdrowszym trybie życia i
-                        zwiększonej pewności siebie? Nasz wykwalifikowany trener osobisty jest
-                        tutaj, aby Ci pomóc!
-                    </p>
-                    <button className="cta">Umów się</button>
-
-                </div>
+				<div className="section1__left"></div>
+				<div className="section1__right">
+					<h1 className="section1__title">Postaw sobie wyzwanie</h1>
+					<p className="section1__text">
+						Czy marzysz o lepszej kondycji fizycznej, zdrowszym trybie życia i
+						zwiększonej pewności siebie? Nasz wykwalifikowany trener osobisty
+						jest tutaj, aby Ci pomóc!
+					</p>
+					<button className="cta">Umów się</button>
+				</div>
 			</section>
 			<section className="section2">
 				<div className="section2__top">
-					<div className="section2__top1"></div>
-					<div className="section2__top2"></div>
-					<div className="section2__top3"></div>
+					<div className="section2__container">
+						<div className="top__img img1"></div>
+						<h4 className="section2__top__title">Siłownia</h4>
+						<p>
+							Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+							Doloribus vitae optio deleniti. Quae iure voluptatum doloribus,
+							repellat voluptas explicabo minus.
+						</p>
+					</div>
+					<div className="section2__container">
+						<div className="top__img img2"></div>
+						<h4 className="section2__top__title">Kettle</h4>
+						<p>
+							Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+							Doloribus vitae optio deleniti. Quae iure voluptatum doloribus,
+							repellat voluptas explicabo minus.
+						</p>
+					</div>
+					<div className="section2__container">
+						<div className="top__img img3"></div>
+						<h4 className="section2__top__title">Fitness</h4>
+						<p>
+							Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+							Doloribus vitae optio deleniti. Quae iure voluptatum doloribus,
+							repellat voluptas explicabo minus.
+						</p>
+					</div>
 				</div>
 				<div className="section2__bottom">
 					<h2 className="section2__title">Twój trening</h2>
@@ -38,7 +60,7 @@ export default function Home() {
 						poprawić swoje samopoczucie ogólne - jesteśmy tutaj, by Ci pomóc na
 						każdym kroku.
 					</p>
-                    <button className="cta">Skontaktuj się ze mną już dziś</button>
+					<button className="cta">Skontaktuj się ze mną już dziś</button>
 				</div>
 			</section>
 			<section className="section3"></section>
